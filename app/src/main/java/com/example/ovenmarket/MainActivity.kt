652b340279a240005cfeec215c9278bb3c8cc26a
jsw6701 <anes53027@gmail.com>
2022-12-10 21:54:16 +0900

    회원가입, 로그인 구현

--- app/src/main/java/com/example/ovenmarket/MainActivity.kt
+++ app/src/main/java/com/example/ovenmarket/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.ovenmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.ovenmarket.mypage.MyPageFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myPageFragment = MyPageFragment()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        replaceFragment(myPageFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.myPage -> replaceFragment(myPageFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.fragment_container, fragment)
+                commit()
+            }
     }
 }
