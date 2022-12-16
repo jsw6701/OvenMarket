fe720424c209e7c1fa5e3c7a566c8eb40d6e6b0f
myeongju00 <j51677767@gmail.com>
2022-12-17 02:57:09 +0900

    feat : 구글 로그인 완성본

--- app/src/main/java/com/example/ovenmarket/LoginActivity.kt
+++ app/src/main/java/com/example/ovenmarket/LoginActivity.kt
@@ -1,39 +1,47 @@
 package com.example.ovenmarket
 
-import android.app.Activity
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
     var mGoogleSignInClient : GoogleSignInClient? = null
+    val database = Firebase.firestore
+
+    private var uid: String? = null
+    private var email: String? = null
+    private var nickName: String? = null
+    private var profileImgUri: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(R.string.firebase_web_client_id.toString())
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
+            .requestProfile()
             .build()
+
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-        firebaseAuth = FirebaseAuth.getInstance()
-        var loginButton = findViewById<SignInButton>(R.id.googleSignInBtn)
+        auth = FirebaseAuth.getInstance()
+
+        val loginButton = findViewById<SignInButton>(R.id.googleSignInBtn)
         loginButton.setOnClickListener {
             googleLogin()
         }
@@ -43,24 +51,57 @@ class LoginActivity : AppCompatActivity() {
         val signInIntent = mGoogleSignInClient!!.signInIntent
         googleLoginLauncher.launch(signInIntent)
     }
-    var googleLoginLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val data = result.data
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            getGoogleInfo(task)
+
+    var googleLoginLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result : ActivityResult ->
+
+        val TAG = "구글 로그인 결과"
+
+        if (result.resultCode == -1) { //result.resultCode == RESULT_OK
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                account.idToken?.let { idToken ->
+                    // 데이터 스토어에 유저 정보 저장
+                    email = account.email
+                    nickName = account.displayName
+                    profileImgUri = account.photoUrl.toString()
+                }
+                firebaseAuthWithGoogle(account.idToken!!)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            catch (e: ApiException) {
+                Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            }
+        } else {
+            Log.d(TAG, "${result.resultCode} , ${result.data}")
+            Log.d(TAG, "${auth}")
         }
     }
-    fun getGoogleInfo(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val TAG = "구글 로그인 결과"
-            val account = completedTask.getResult(ApiException::class.java)
-            Log.d(TAG, account.id!!)
-            Log.d(TAG, account.familyName!!)
-            Log.d(TAG, account.givenName!!)
-            Log.d(TAG, account.email!!)
-        }
-        catch (e: ApiException) {
-            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
-        }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val TAG = "구글 로그인 결과"
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        database.collection("users")
+            .add(User(user!!.uid, email, nickName, profileImgUri))
     }
 }
